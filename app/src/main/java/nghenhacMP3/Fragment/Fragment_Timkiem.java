c029de1c478d977033dc39b26de2115175a42dd8
phanhamb <maiyeu619@gmail.com>
2019-11-13 00:14:06 +0700

    Initial commit

---
+++ app/src/main/java/nghenhacMP3/Fragment/Fragment_Timkiem.java
@@ -0,0 +1,22 @@
+package nghenhacMP3.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.Activity.R;
+
+public class Fragment_Timkiem extends Fragment {
+    @Nullable
+
+    View view;
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_timkiem,container,false);
+        return view;
+    }
+}
